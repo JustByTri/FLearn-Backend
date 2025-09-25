709d934cb71a9ddc92b4951acea91cf6fe7993c2
xbensieve <bennguyen.contact@gmail.com>
2025-09-26 02:25:16 +0700

    feat: manage goals for each course.

---
+++ Common/DTO/Goal/Response/GoalResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Common.DTO.Goal.Response
+{
+    public class GoalResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
--- Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a91e824dae0d0faae19063988705b059a768a35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dca7cc9e9ca6b372fbe5b8815f76f796b71aa9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
